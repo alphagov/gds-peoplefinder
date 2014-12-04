cab1ccc2fa32610c994c16e7d70901ac02076a3e
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-12-04 14:34:23 +0000

    Need to configure SES server as well

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,3 +1,4 @@
 ActionMailer::Base.add_delivery_method :aws_ses, AWS::SES::Base,
-  access_key_id: ENV['AMAZON_ACCESS_KEY'],
-  secret_access_key: ENV['AMAZON_SECRET_KEY']
+  access_key_id: ENV['AWS_SES_ACCESS_KEY'],
+  secret_access_key: ENV['AWS_SES_SECRET_KEY'],
+  server: ENV['AWS_SES_SERVER']
